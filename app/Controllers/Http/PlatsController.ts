86f5349c109662a9f9751b8eccedb74dc3aaadeb
Zengry10 <remi.lemoust-de-lafosse@epitech.digital>
2023-01-25 15:43:27 +0100

    now we can only choose max of one drink

--- app/Controllers/Http/PlatsController.ts
+++ app/Controllers/Http/PlatsController.ts
@@ -10,26 +10,38 @@ export default class PlatsController {
 
     const payload = await request.validate(ValidatorPlat)
 
-    // Crée un nouveau plat en utilisant les données de la requête
-    const plat = await Plat.create(payload)
-
     // Si les ingrédients sont inclus dans la requête
     if (request.input('drinks')) {
       // Récupère les ingrédients de la requête
       const drinks = request.input('drinks')
 
-      // Boucle sur les ingrédients pour les attacher au plat
-      for (let drink of drinks) {
-        await Database.table('create_plat_drink_tables').insert({
-          plat_id: plat.id,
-          drink_id: drink
+      // vérifie si la longueur des boissons sélectionnées est égale à 1
+      if (drinks.length === 1 || drinks.length === 0) {
+        // Crée un nouveau plat en utilisant les données de la requête
+        const plat = await Plat.create(payload)
+        // Boucle sur les ingrédients pour les attacher au plat
+        for (let drink of drinks) {
+          await Database.table('create_plat_drink_tables').insert({
+            plat_id: plat.id,
+            drink_id: drink
+          })
+        }
+        // Renvoie un status 201 (Créé) et les détails du plat créé
+        return response.status(201).json(plat)
+      } else {
+        // Renvoie une erreur si plus d'une boisson est sélectionnée
+        return response.status(400).json({
+          message: 'Vous ne pouvez sélectionner qu\'une seule boisson pour un menu'
         })
       }
+    } else {
+      // Crée un nouveau plat en utilisant les données de la requête
+      const plat = await Plat.create(payload)
+      // Renvoie un status 201 (Créé) et les détails du plat créé
+      return response.status(201).json(plat)
     }
-
-    // Renvoie un status 201 (Créé) et les détails du plat créé
-    return response.status(201).json(plat)
   }
+
 
 
 
