dae26c508f0fedabae9342bdae1bc0166e7db60b
Zengry10 <remi.lemoust-de-lafosse@epitech.digital>
2023-02-02 16:41:17 +0100

    change function read menu

--- app/Controllers/Http/MenusController.ts
+++ app/Controllers/Http/MenusController.ts
@@ -77,108 +77,86 @@ export default class MenusController {
   
 
   
-  public async show({ params }: HttpContextContract) {
+  public async show({ params, response }: HttpContextContract) {
     const menu = await Menu.findOrFail(params.id)
     await menu.load('drinks')
     await menu.load('complements')
-    await menu.load('burgers')
-    return menu
+    await menu.load('burgers', (burgerQuery) => {
+      burgerQuery.preload('ingredients')
+    })
+
+    return response.status(201).json({
+      status: 201,
+      data: menu,
+    })
   }
 
 
-  // public async index({ response }: HttpContextContract) {
-  //   const menus = await Menu.all()
-  //   const menusBurgers = await Database.from('create_menu_burger_tables')
-  //   const burgers = await Database.from('burgers')
-  //   const burgerIngredients = await Database.from('create_burger_ingredient_tables')
-  //   const ingredients = await Database.from('ingredients')
-  //   const data = menus.map((menu: any) => {
-  //     const menuBurgers = menusBurgers.filter((menuBurger: any) => menuBurger.menu_id === menu.id)
-  //     const menuBurgersId = menuBurgers.map((menuBurger: any) => menuBurger.burger_id)
-  //     const menuBurgersName = burgers.filter((burger: any) => menuBurgersId.includes(burger.id))
-  //     const burgerWithIngredients = menuBurgersName.map((burger: any) => {
-  //       const burgerIngredientsId = burgerIngredients.filter((burgerIngredient: any) => burgerIngredient.burger_id === burger.id)
-  //       const ingredientsId = burgerIngredientsId.map((burgerIngredient: any) => burgerIngredient.ingredient_id)
-  //       const ingredientsName = ingredients.filter((ingredient: any) => ingredientsId.includes(ingredient.id))
-  //       return {
-  //         id: burger.id,
-  //         name: burger.name,
-  //         price: burger.price,
-  //         stock: burger.stock,
-  //         picture: burger.picture,
-  //         ingredients: ingredientsName
-  //       }
-  //     })
-  //     return {
-  //       id: menu.id,
-  //       name: menu.name,
-  //       price: menu.price,
-  //       stock: menu.stock,
-  //       picture: menu.picture,
-  //       burgers: burgerWithIngredients
-  //     }
-  //   })
-  //   return response.status(201).json({
-  //     status: 201,
-  //     data: data,
-  //     menus: menus,
-  //   })
-  // }
 
   public async index({ response }: HttpContextContract) {
-    const menus = await Menu.all()
-    const menusBurgers = await Database.from('create_menu_burger_tables')
-    const burgers = await Database.from('burgers')
-    const burgerIngredients = await Database.from('create_burger_ingredient_tables')
-    const ingredients = await Database.from('ingredients')
-  
-    const menusDrinks = await Database.from('create_menu_drink_tables')
-    const drinks = await Database.from('drinks')
-  
-    const menusComplements = await Database.from('create_menu_complement_tables')
-    const complements = await Database.from('complements')
-  
-    const data = menus.map((menu: any) => {
-      const menuBurgers = menusBurgers.filter((menuBurger: any) => menuBurger.menu_id === menu.id)
-      const menuBurgersId = menuBurgers.map((menuBurger: any) => menuBurger.burger_id)
-      const menuBurgersName = burgers.filter((burger: any) => menuBurgersId.includes(burger.id))
-      const burgerWithIngredients = menuBurgersName.map((burger: any) => {
-        const burgerIngredientsId = burgerIngredients.filter((burgerIngredient: any) => burgerIngredient.burger_id === burger.id)
-        const ingredientsId = burgerIngredientsId.map((burgerIngredient: any) => burgerIngredient.ingredient_id)
-        const ingredientsName = ingredients.filter((ingredient: any) => ingredientsId.includes(ingredient.id))
-        return {
-          id: burger.id,
-          name: burger.name,
-          price: burger.price,
-          stock: burger.stock,
-          picture: burger. picture,
-          ingredients: ingredientsName
-        }
-      })
-  
-      const menuDrinks = menusDrinks.filter((menuDrink: any) => menuDrink.menu_id === menu.id)
-      const menuDrinksId = menuDrinks.map((menuDrink: any) => menuDrink.drink_id)
-      const menuDrinksName = drinks.filter((drink: any) => menuDrinksId.includes(drink.id))
-  
-      const menuComplements = menusComplements.filter((menuComplement: any) => menuComplement.menu_id === menu.id)
-      const menuComplementsId = menuComplements.map((menuComplement: any) => menuComplement.complement_id)
-      const menuComplementsName = complements.filter((complement: any) => menuComplementsId.includes(complement.id))
-  
-      return {
-        id: menu.id,
-        name: menu.name,
-        price: menu.price,
-        stock: menu.stock,
-        picture: menu.picture,
-        burgers: burgerWithIngredients,
-        drinks: menuDrinksName,
-        complements: menuComplementsName
-      }
-    })
+    const data = await Menu.query().preload('burgers', (burgerQuery) => {
+      burgerQuery.preload('ingredients')
+    }).preload('drinks').preload('complements').exec()
+
     return response.status(201).json({
       status: 201,
       data: data,
-    })
+  })
+
+  
+    // const menus = await Menu.all()
+    // const menusBurgers = await Database.from('create_menu_burger_tables')
+    // const burgers = await Database.from('burgers')
+    // const burgerIngredients = await Database.from('create_burger_ingredient_tables')
+    // const ingredients = await Database.from('ingredients')
+  
+    // const menusDrinks = await Database.from('create_menu_drink_tables')
+    // const drinks = await Database.from('drinks')
+  
+    // const menusComplements = await Database.from('create_menu_complement_tables')
+    // const complements = await Database.from('complements')
+  
+    // const data = menus.map((menu: any) => {
+    //   const menuBurgers = menusBurgers.filter((menuBurger: any) => menuBurger.menu_id === menu.id)
+    //   const menuBurgersId = menuBurgers.map((menuBurger: any) => menuBurger.burger_id)
+    //   const menuBurgersName = burgers.filter((burger: any) => menuBurgersId.includes(burger.id))
+    //   const burgerWithIngredients = menuBurgersName.map((burger: any) => {
+    //     const burgerIngredientsId = burgerIngredients.filter((burgerIngredient: any) => burgerIngredient.burger_id === burger.id)
+    //     const ingredientsId = burgerIngredientsId.map((burgerIngredient: any) => burgerIngredient.ingredient_id)
+    //     const ingredientsName = ingredients.filter((ingredient: any) => ingredientsId.includes(ingredient.id))
+    //     return {
+    //       id: burger.id,
+    //       name: burger.name,
+    //       price: burger.price,
+    //       stock: burger.stock,
+    //       picture: burger. picture,
+    //       ingredients: ingredientsName
+    //     }
+    //   })
+  
+    //   const menuDrinks = menusDrinks.filter((menuDrink: any) => menuDrink.menu_id === menu.id)
+    //   const menuDrinksId = menuDrinks.map((menuDrink: any) => menuDrink.drink_id)
+    //   const menuDrinksName = drinks.filter((drink: any) => menuDrinksId.includes(drink.id))
+  
+    //   const menuComplements = menusComplements.filter((menuComplement: any) => menuComplement.menu_id === menu.id)
+    //   const menuComplementsId = menuComplements.map((menuComplement: any) => menuComplement.complement_id)
+    //   const menuComplementsName = complements.filter((complement: any) => menuComplementsId.includes(complement.id))
+  
+    //   return {
+    //     id: menu.id,
+    //     name: menu.name,
+    //     price: menu.price,
+    //     stock: menu.stock,
+    //     picture: menu.picture,
+    //     burgers: burgerWithIngredients,
+    //     drinks: menuDrinksName,
+    //     complements: menuComplementsName
+    //   }
+    // })
+    // return response.status(201).json({
+    //   status: 201,
+    //   data: data,
+    // })
   }
   
 }
